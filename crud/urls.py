8ecfdf1fe545e144c398457af2c81f1ba96505aa
Alinson Lopez <alinsonl11@gmail.com>
2024-04-20 21:27:18 -0400

    gitignore

---
+++ crud/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # Registrar las URLs de tu aplicación aquí
+    path('', include('apps.movies.urls')),
+]
+
+# Para servir archivos estáticos en desarrollo
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
